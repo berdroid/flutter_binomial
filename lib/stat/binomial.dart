a08d9e1c1fa9c669993e9538078d6cead3dfffb6
Bernhard Bender <ber.droid@googlemail.com>
2021-02-28 00:36:09 +0100

    Rewrote constructor to avoid O(n²) behaviour when calculating cumulative distribution

--- lib/stat/binomial.dart
+++ lib/stat/binomial.dart
@@ -8,18 +8,24 @@ class Binomial {
 
   final double mu;
   final double sigma;
-  List<double> get pd => _pd;
-  List<double> get cd => _cd;
+  List<double> get pd => List<double>.unmodifiable(_pd);
+  List<double> get cd => List<double>.unmodifiable(_cd);
 
-  List<double> _pd;
-  List<double> _cd;
+  final List<double> _pd;
+  final List<double> _cd;
 
   Binomial(this.n, this.p)
       : mu = n * p,
-        sigma = sqrt(n * p * (1 - p)) {
-    _pd = List<double>.unmodifiable(List<double>.generate(n + 1, (k) => bernoulli(n, p, k)));
-    _cd = List<double>.unmodifiable(
-        List<double>.generate(n + 1, (k) => _pd.sublist(0, k + 1).fold<double>(0.0, (a, b) => a + b)));
+        sigma = sqrt(n * p * (1 - p)),
+        _pd = List<double>.filled(n + 1, null),
+        _cd = List<double>.filled(n + 1, null) {
+    double sum = 0.0;
+    Iterable.generate(n + 1).forEach((k) {
+      final prob = bernoulli(n, p, k);
+      sum += prob;
+      _pd[k] = prob;
+      _cd[k] = sum;
+    });
   }
 
   bool inSigma(int k, [int s = 1]) => mu - s * sigma <= k && k <= mu + s * sigma;
