fd12ce16ceab839ccf2fd51b5449cc8125a26ee9
Bernhard Bender <ber.droid@googlemail.com>
2021-02-27 23:31:33 +0100

    Added test cases for probability mass and cumulative distributions

--- test/stat/binomi_test.dart
+++ test/stat/binomi_test.dart
@@ -2,6 +2,46 @@ import 'package:binomi/stat/binomial.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  group('Binomial distribution', () {
+    final b1 = Binomial(5, 0.5);
+    final b2 = Binomial(3, 0.05);
+
+    test('Probability n=5, p=0.5', () {
+      expect(b1.pd.length, 6);
+      expect(b1.pd[0], 0.03125);
+      expect(b1.pd[1], 0.15625);
+      expect(b1.pd[2], 0.3125);
+      expect(b1.pd[3], 0.3125);
+      expect(b1.pd[4], 0.15625);
+      expect(b1.pd[5], 0.03125);
+    });
+
+    test('Cumulative n=5, p=0.5', () {
+      expect(b1.cd.length, 6);
+      expect(b1.cd[0], 0.03125);
+      expect(b1.cd[1], 0.1875);
+      expect(b1.cd[2], 0.5);
+      expect(b1.cd[3], 0.8125);
+      expect(b1.cd[4], 0.96875);
+      expect(b1.cd[5], 1.0);
+    });
+
+    test('Probability n=3, p=0.05', () {
+      expect(b2.pd.length, 4);
+      expect(b2.pd[0], closeTo(0.857375, 0.0000001));
+      expect(b2.pd[1], closeTo(0.135375, 0.0000001));
+      expect(b2.pd[2], closeTo(0.007125, 0.0000001));
+      expect(b2.pd[3], closeTo(0.000125, 0.0000001));
+    });
+
+    test('Cumulative n=3, p=0.05', () {
+      expect(b2.cd.length, 4);
+      expect(b2.cd[0], closeTo(0.857375, 0.0000001));
+      expect(b2.cd[1], closeTo(0.99275, 0.0000001));
+      expect(b2.cd[2], closeTo(0.999875, 0.0000001));
+      expect(b2.cd[3], closeTo(1.0, 0.0000001));
+    });
+  });
   group('µ and σ', () {
     test('n=5, p=0.5', () {
       final bin = Binomial(5, 0.5);
